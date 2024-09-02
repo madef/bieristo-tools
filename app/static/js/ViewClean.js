d783e6f587d0b8f07541334154bb143ad53bac67
Maxence de Flotte <maxence@deflotte.fr>
2024-09-02 17:25:20 +0200

    Feature: ajout du dosage de sucre

--- app/static/js/ViewClean.js
+++ app/static/js/ViewClean.js
@@ -7,139 +7,92 @@ import History from './History.js'
 import Unit from './Unit.js'
 
 class ViewClean {
-  constructor ($content) {
-    this.productList = [
-      {
-        code: 'san',
-        label: Translator.__('ViewClean:San:label'),
-        unit: Translator.__('ViewClean:San:unit'),
-        rate: {
-          min: 1.5,
-          max: 2.5
+  constructor ($content, subview) {
+    switch (subview) {
+      case 'OXY':
+        this.product = {
+          code: 'oxy',
+          label: Translator.__('ViewClean:Oxy:label'),
+          unit: Translator.__('ViewClean:Oxy:unit'),
+          rate: {
+            min: 4,
+            max: 4
+          }
         }
-      },
-      {
-        code: 'oxy',
-        label: Translator.__('ViewClean:Oxy:label'),
-        unit: Translator.__('ViewClean:Oxy:unit'),
-        rate: {
-          min: 4,
-          max: 4
+        break
+      case 'PBW':
+        this.product = {
+          code: 'pbw',
+          label: Translator.__('ViewClean:Pbw:label'),
+          unit: Translator.__('ViewClean:Pbw:unit'),
+          rate: {
+            min: 5,
+            max: 15
+          }
         }
-      },
-      {
-        code: 'pwb',
-        label: Translator.__('ViewClean:Pwb:label'),
-        unit: Translator.__('ViewClean:Pwb:unit'),
-        rate: {
-          min: 5,
-          max: 15
+        break
+      case 'WASH':
+        this.product = {
+          code: 'wash',
+          label: Translator.__('ViewClean:Wash:label'),
+          unit: Translator.__('ViewClean:Wash:unit'),
+          rate: {
+            min: 7,
+            max: 21
+          }
         }
-      },
-      {
-        code: 'wash',
-        label: Translator.__('ViewClean:Wash:label'),
-        unit: Translator.__('ViewClean:Wash:unit'),
-        rate: {
-          min: 7,
-          max: 21
+        break
+      case 'CIP':
+        this.product = {
+          code: 'cip',
+          label: Translator.__('ViewClean:Cip:label'),
+          unit: Translator.__('ViewClean:Cip:unit'),
+          rate: {
+            min: 2.5,
+            max: 4
+          }
         }
-      },
-      {
-        code: 'cip',
-        label: Translator.__('ViewClean:Cip:label'),
-        unit: Translator.__('ViewClean:Cip:unit'),
-        rate: {
-          min: 2.5,
-          max: 4
+        break
+      case 'ACID':
+        this.product = {
+          code: 'acid',
+          label: Translator.__('ViewClean:Acid:label'),
+          unit: Translator.__('ViewClean:Acid:unit'),
+          rate: {
+            min: 10,
+            max: 10
+          }
         }
-      },
-      {
-        code: 'acid',
-        label: Translator.__('ViewClean:Cip:label'),
-        unit: Translator.__('ViewClean:Cip:unit'),
-        rate: {
-          min: 10,
-          max: 10
+        break
+      default:
+        this.product = {
+          code: 'san',
+          label: Translator.__('ViewClean:San:label'),
+          unit: Translator.__('ViewClean:San:unit'),
+          rate: {
+            min: 1.5,
+            max: 2.5
+          }
         }
-      }
-    ]
+        break
+    }
 
-    this.history = new History('ViewClean')
+    this.history = new History(`ViewClean:${this.product.code}`)
 
     this.unit = Unit.getInstance()
     this.unit.addChangeObserver('view', (unitType) => {
-      this.productList.forEach((product) => {
-        this.updateResult(product, 'staged')
-      })
+      this.updateResult()
     })
 
     this.view = new Brique(`<div class="flex flex-col gap-4">
-        <div class="grid md:grid-cols-2 gap-2" data-var="form">
-          <div>
-            <label for="san" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:San:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="san" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="sanEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="sanResult">
-              </div>
+        <div>
+          <label for="product" class="block text-sm font-medium leading-6">${this.product.label}</label>
+          <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
+            <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
+              <input type="number" autocomplete="off" id="product" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="productEntry">
+              <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
             </div>
-          </div>
-          <div>
-            <label for="pwb" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:Pwb:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="pwb" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="pwbEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="pwbResult">
-              </div>
-            </div>
-          </div>
-          <div>
-            <label for="oxy" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:Oxy:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="oxy" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="oxyEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="oxyResult">
-              </div>
-            </div>
-          </div>
-          <div>
-            <label for="wash" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:Wash:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="wash" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="washEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="washResult">
-              </div>
-            </div>
-          </div>
-          <div>
-            <label for="cip" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:Cip:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="cip" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="cipEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="cipResult">
-              </div>
-            </div>
-          </div>
-          <div>
-            <label for="acid" class="block text-sm font-medium leading-6">${Translator.__('ViewClean:Acid:label')}</label>
-            <div class="relative mt-2 rounded-md shadow-sm flex flex-wrap md:flex-nowrap gap-2">
-              <div class="flex w-full md:w-1/2 items-center gap-2 rounded-md border border-white pr-2 group hover:border-amber-500 focus-within:border-amber-500">
-                <input type="number" autocomplete="off" id="acid" class="rounded-md py-1 px-2 bg-transparent w-full text-lg focus:outline-none" data-var="acidEntry">
-                <div class="pointer-events-none" data-var="volumeUnit">${this.unit.get('volume').shortLabel}</div>
-              </div>
-              <div class="flex items-center w-1/2 gap-2 overflow-x-auto" data-var="acidResult">
-              </div>
+            <div class="flex items-center w-full md:w-1/2 gap-2 overflow-x-auto" data-var="productResult">
             </div>
           </div>
         </div>
@@ -148,10 +101,8 @@ class ViewClean {
       </div>`)
       .appendTo($content, true)
 
-    this.productList.forEach((product) => {
-      this.view.addEventListener(`${product.code}Entry`, ['keyup', 'change'], (e) => {
-        this.updateResult(product, e.type === 'change' ? 'final' : 'staged')
-      })
+    this.view.addEventListener('productEntry', 'keyup', (e) => {
+      this.updateResult()
     })
 
     this.renderHistory()
@@ -159,70 +110,79 @@ class ViewClean {
   }
 
   setDefaultValue () {
-    this.productList.forEach((product) => {
-      const lastHistory = this.getLastHistory(product.code)
-      if (lastHistory) {
-        this.view.get(`${product.code}Entry`).value = lastHistory.value
-        this.updateResult(product)
-      }
-    })
+    const lastHistory = this.getLastHistory()
+    if (lastHistory) {
+      this.view.get('productEntry').value = lastHistory.value
+      this.updateResult()
+    }
   }
 
   getLastHistory (productCode) {
-    for (const history of this.history.get()) {
-      if (history.productCode === productCode) {
-        return history
-      }
+    const history = this.history.get()
+
+    if (history.length) {
+      return history[0]
     }
 
     return null
   }
 
-  cleanLastStagedHistory (productCode) {
-    const historyList = this.history.get()
-    for (const historyKey in historyList) {
-      const history = historyList[historyKey]
-      if (history.productCode === productCode) {
-        if (history.status === 'staged') {
-          this.history.removeRow(historyKey)
-        }
-        return
-      }
-    }
-  }
-
-  updateResult (product, status) {
+  updateResult () {
     const volume = this.unit.get('volume')
-    let value = parseFloat(this.view.get(`${product.code}Entry`).value)
+    let value = parseFloat(this.view.get('productEntry').value)
 
     if (isNaN(value)) {
       value = 0.0
     }
 
-    const minResult = this.round(volume.convert(value).L * product.rate.min)
-    const maxResult = this.round(volume.convert(value).L * product.rate.max)
+    const minResult = this.round(volume.convert(value).L * this.product.rate.min)
+    const maxResult = this.round(volume.convert(value).L * this.product.rate.max)
 
     this.view.forEach('volumeUnit', $unit => { $unit.innerText = volume.shortLabel })
 
-    this.view
-      .empty(`${product.code}Result`)
-      .append(`${product.code}Result`, new Brique(`<div class="bg-cyan-950 rounded-md p-2">${minResult} ${product.unit}</div>`))
+    this.view.empty('productResult')
+    this.view.append(
+      'productResult',
+      new Brique(`<div class="bg-cyan-950 rounded-md p-2 flex gap-2 w-full md:w-auto">
+  <div class="grow">${minResult}</div>
+  <div>${this.product.unit}</div>
+  <button class="hover:text-amber-500" data-var="history-add" title="${Translator.__('Generic:Action:historyAdd')}" aria-label="${Translator.__('Generic:Action:historyAdd')}">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+    </svg>
+  </button>
+</div>`)
+        .addEventListener('history-add', 'click', () => {
+          addToHistory()
+        })
+    )
 
-    if (product.rate.min !== product.rate.max) {
-      this.view.append(`${product.code}Result`, new Brique(`<div class="bg-cyan-950 rounded-md p-2">${maxResult} ${product.unit}</div>`))
+    if (this.product.rate.min !== this.product.rate.max) {
+      this.view.append(
+        'productResult',
+        new Brique(`<div class="bg-cyan-950 rounded-md p-2 flex gap-2 w-full md:w-auto">
+  <div class="grow">${maxResult}</div>
+  <div>${this.product.unit}</div>
+  <button class="hover:text-amber-500" data-var="history-add" title="${Translator.__('Generic:Action:historyAdd')}" aria-label="${Translator.__('Generic:Action:historyAdd')}">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+    </svg>
+  </button>
+</div>`)
+          .addEventListener('history-add', 'click', () => {
+            addToHistory()
+          })
+      )
     }
 
-    if (value !== 0 && typeof status !== 'undefined') {
-      const lastHistory = this.getLastHistory(product.code)
-      this.cleanLastStagedHistory(product.code)
+    const addToHistory = () => {
+      const display = minResult === maxResult ? `${value}${volume.shortLabel} => ${minResult}${this.product.unit}` : `${value}${volume.shortLabel} => ${minResult}${this.product.unit} - ${maxResult}${this.product.unit}`
 
-      if (!lastHistory || lastHistory.status === 'staged' || lastHistory.value.toString() !== value.toString() || lastHistory.unit.toString() !== volume.code.toString()) {
+      if (!this.getLastHistory() || this.getLastHistory().display !== display) {
         this.history.addRow({
           value,
-          productCode: product.code,
-          status,
           unit: volume.code,
-          display: minResult === maxResult ? `[${product.label}] ${value}${volume.shortLabel} => ${minResult}${product.unit}` : `[${product.label}] ${value}${volume.shortLabel} => ${minResult}${product.unit} - ${maxResult}${product.unit}`
+          display
         })
       }
     }
@@ -237,15 +197,9 @@ class ViewClean {
       this.view.get('history'),
       this.history,
       (historyRow) => {
-        let product
-        for (product of this.productList) {
-          if (product.code === historyRow.code) {
-            break
-          }
-        }
         this.unit.set('volume', historyRow.unit)
-        this.view.get(`${historyRow.productCode}Entry`).value = historyRow.value
-        this.updateResult(product)
+        this.view.get('productEntry').value = historyRow.value
+        this.updateResult()
       }
     )
   }
