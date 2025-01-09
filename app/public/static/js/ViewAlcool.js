9a23b904a57c546aa6cfe5da2d8d01c2ecafb381
Maxence de Flotte <maxence@deflotte.fr>
2025-01-09 21:53:17 +0100

    Fix: alcool with no sugar defined

--- app/public/static/js/ViewAlcool.js
+++ app/public/static/js/ViewAlcool.js
@@ -174,7 +174,8 @@ class ViewAlcool {
   updateResult () {
     let di = parseFloat(this.view.get('di').value)
     let df = parseFloat(this.view.get('df').value)
-    const sugar = parseFloat(this.view.get('sugar').value)
+    const sugar = parseFloat(this.view.get('sugar').value || 0)
+      console.log(sugar)
     const gravity = this.unit.get('gravity')
     const volume = this.unit.get('volume')
 
